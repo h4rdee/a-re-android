9d1d1e172e8aa683fdc856bebf7fdfcb70f4cd7f
hardee <shegurov.danil@gmail.com>
2022-06-13 09:36:52 +0400

    ref(main): moved ECoreElements out of main

---
+++ utils.py
@@ -0,0 +1,13 @@
+from enum import IntEnum
+
+# core elements ids that we will need throughout g_app lifecycle
+class ECoreElements(IntEnum):
+    NONE = 0,
+    LOGGER = 1,
+    APK_INFO_LABEL = 2,
+    APK_ICON_VIEW = 3,
+    PROGRESS_BAR = 4,
+    TAB_CONTROL_ROOT = 5,
+    TAB_CONTROL_PLUGINS = 6,
+    GENERAL_TAB = 7,
+    PLUGINS_TAB = 8,
