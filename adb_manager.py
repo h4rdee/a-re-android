8798cb81ed9a7d4d58672df144073f74b5317183
hardee <shegurov.danil@gmail.com>
2022-06-14 13:19:11 +0400

    ref(apk/adb): moved callbacks to respective classes

--- adb_manager.py
+++ adb_manager.py
@@ -1,5 +1,5 @@
+from threading import Thread
 import tkinter as tk
-import callbacks
 
 from logger import g_logger
 from functools import partial
@@ -15,6 +15,7 @@ class AdbManager:
         self.gui = gui
         self.ui_root = gui.get_adb_tab()
         self.logger = g_logger
+        self.shell_command = tk.StringVar(self.ui_root, '> ')
         self.is_connected = False
         self.construct()
         
@@ -22,46 +23,70 @@ class AdbManager:
         return self.gui
 
     def construct(self) -> None:
-        self.gui.create_label(
-            self.ui_root, 20, 20, 'IP:'
+        self.settings_tab = self.gui.create_tab(
+            self.gui.get_adb_tab_bar(), 520, 360, 'Settings'
         )
 
-        self.adb_server_ip = tk.StringVar(self.ui_root)
-        self.adb_server_port = tk.StringVar(self.ui_root)
+        self.shell_tab = self.gui.create_tab(
+            self.gui.get_adb_tab_bar(), 520, 360, 'Shell'
+        )
+
+        self.gui.create_label(
+            self.settings_tab, 20, 20, 'IP:'
+        )
+
+        self.adb_server_ip = tk.StringVar(self.settings_tab)
+        self.adb_server_port = tk.StringVar(self.settings_tab)
 
         self.adb_server_ip.set('127.0.0.1')
         self.adb_server_port.set('5037')
 
         self.gui.create_editbox(
-            self.ui_root, 20, 40, 
+            self.settings_tab, 20, 40, 
             self.adb_server_ip, 100
         )
 
         self.gui.create_label(
-            self.ui_root, 130, 20, 'Port:'
+            self.settings_tab, 130, 20, 'Port:'
         )
 
         self.gui.create_editbox(
-            self.ui_root, 130, 40, 
+            self.settings_tab, 130, 40, 
             self.adb_server_port, 50
         )
 
         self.gui.create_button(
-            self.ui_root, 20, 80, 'Connect to ADB server',
-            partial(callbacks.adb_connect_callback, self), 160
+            self.settings_tab, 20, 80, 'Connect to ADB server',
+            partial(self.adb_connect_callback, self), 160
         )
 
         self.devices_label = self.gui.create_label(
-            self.ui_root, 20, 120, 
+            self.settings_tab, 20, 120, 
             'Devices list: (connect to adb server first)',
             ECoreElements.ADB_DEVICES_LABEL
         )
 
         self.devices_cbx = self.gui.create_combobox(
-            self.ui_root, 20, 140, [],
-            partial(callbacks.selected_device_changed_callback, self),
+            self.settings_tab, 20, 140, [],
+            partial(self.selected_device_changed_callback, self),
             ECoreElements.ADB_DEVICES
         )
+
+        self.devices_cbx.focus_set()
+
+        self.shell_label = self.gui.create_label(
+            self.shell_tab, 20, 20, 
+            'Device shell (connect to adb server and select device first):',
+            ECoreElements.ADB_SHELL_LABEL
+        )
+
+        self.shell_terminal = self.gui.create_terminal(
+            self.shell_tab, 20, 40, 480, 300, 
+            ECoreElements.ADB_SHELL_TERMINAL
+        )
+
+        self.shell_terminal.insert('end', '> ')
+        self.shell_terminal.focus_set()
 
         self.devices_cbx["state"] = "disabled"
 
@@ -101,3 +126,80 @@ class AdbManager:
 
     def set_selected_device(self, serial) -> None:
         self.selected_device = serial
+
+    def adb_connect_callback(self, __none__=None, threaded=False) -> None:
+        if threaded == False: # hack, i'm too lazy to asyncify tkinter
+            thread = Thread(
+                target=self.adb_connect_callback, 
+                args=(self, True,)
+            )
+            thread.start()
+            return
+
+        gui = self.get_gui()
+        logger = gui.get_logger()
+
+        server_ip = self.get_adb_server_ip()
+        server_port = self.get_adb_server_port()
+
+        logger.info(
+            f'[>] [adb] Connecting to {server_ip}:{server_port}..\n'
+        )
+
+        self.connect(server_ip, int(server_port))
+
+        devices_label = gui.get_element_by_opt_id(
+            ECoreElements.ADB_DEVICES_LABEL
+        )
+
+        shell_label = gui.get_element_by_opt_id(
+            ECoreElements.ADB_SHELL_LABEL
+        )
+
+        devices_cbx = gui.get_element_by_opt_id(
+            ECoreElements.ADB_DEVICES
+        )
+
+        devices = self.get_devices_list()
+
+        if len(devices) == 0:
+            devices_label.config(text='< no devices >')
+            self.set_selected_device(None)
+            return
+
+        devices_label.config(text='Devices list:')
+
+        devices_cbx["state"] = "readonly"
+        devices_cbx["values"] = devices
+        devices_cbx.current(0)
+
+        self.set_selected_device(devices_cbx.get())
+
+        shell_label.config(text=f'Shell ({devices_cbx.get()}):')
+
+    def selected_device_changed_callback(self, __none__=None, threaded=False) -> None:
+        if threaded == False: # hack, i'm too lazy to asyncify tkinter
+            thread = Thread(
+                target=self.selected_device_changed_callback, 
+                args=(self, True,)
+            )
+            thread.start()
+            return
+
+        gui = self.get_gui()
+        logger = gui.get_logger()
+
+        devices_cbx = gui.get_element_by_opt_id(
+            ECoreElements.ADB_DEVICES
+        )
+
+        shell_label = gui.get_element_by_opt_id(
+            ECoreElements.ADB_SHELL_LABEL
+        )
+
+        selected_device = devices_cbx.get()
+        self.set_selected_device(selected_device)
+
+        shell_label.config(text=f'Shell ({selected_device}):')
+
+        logger.info(f'[>] [adb] Changed device to {selected_device}\n')
--- apk_manager.py
+++ apk_manager.py
@@ -1,8 +1,16 @@
-import callbacks
+import os
+import subprocess
 
 from functools import partial
+from pathlib import Path
 from gui import elements_layout as el
 from utils import ECoreElements
+from threading import Thread
+from tkinter import filedialog
+from apk_parser import ApkParser
+
+g_apk_parser = None
+g_current_file = None
 
 class ApkManager:
     def __init__(self, gui) -> None:
@@ -13,18 +21,20 @@ class ApkManager:
     def construct(self) -> None:
         self.gui.create_button(
             self.ui_root, 20, 20, "Decompile APK", 
-            partial(callbacks.decompile_apk_callback, self.gui),
+            self.decompile_apk_callback,
             el["button"]["width"]
         )
 
         self.gui.create_button(
             self.ui_root, 20 + el["button"]["width"] * 1, 20, "Re-build APK",  
-            partial(callbacks.recompile_apk_callback, self.gui), el["button"]["width"]
+            self.recompile_apk_callback, el["button"]["width"]
         )
 
-        self.gui.create_label(
+        apk_info_label = self.gui.create_label(
             self.ui_root, 20, 20 + 50, 'APK info:'
         )
+
+        apk_info_label.focus_set()
 
         self.gui.create_label(
             self.ui_root, 20, 20 + 70, '< Load APK >',
@@ -40,3 +50,123 @@ class ApkManager:
             self.ui_root, -6, -7, 0, 0,
             ECoreElements.PROGRESS_BAR
         )
+
+    def decompile_apk_callback(self, __none__=None, threaded=False) -> None:
+        if threaded == False: # hack, i'm too lazy to asyncify tkinter
+            thread = Thread(
+                target=self.decompile_apk_callback, 
+                args=(self, True,)
+            )
+            thread.start()
+            return
+
+        global g_current_file, g_apk_parser
+
+        file_path = filedialog.askopenfilename()
+
+        if file_path == '': return
+
+        _, file_name = os.path.split(file_path)
+        g_current_file = file_path
+
+        info_label = self.gui.get_element_by_opt_id(
+            ECoreElements.APK_INFO_LABEL
+        )
+
+        icon_view = self.gui.get_element_by_opt_id(
+            ECoreElements.APK_ICON_VIEW
+        )
+
+        progressbar = self.gui.get_element_by_opt_id(
+            ECoreElements.PROGRESS_BAR
+        )
+
+        progressbar.start()
+
+        g_apk_parser = ApkParser(g_current_file, info_label, icon_view)
+
+        if g_apk_parser.parse() != True:
+            progressbar.stop()
+            self.gui.get_logger().error(f"[-] Failed to decompile: {g_current_file}\n")
+            return
+
+        self.gui.get_root().title(f'A-RE: Android ({g_current_file})')
+
+        decompile_cmd = f'java -Xmx1024m -jar {str(Path.cwd())}\\vendor\\apktool\\apktool.jar' \
+            f" d {g_current_file} -o {str(Path.cwd())}\\decompiled\\{file_name}_decompiled -f"
+
+        # decompiling file
+        with subprocess.Popen(
+            decompile_cmd, shell=False, stdout=subprocess.PIPE, 
+            bufsize=1, universal_newlines=True
+        ) as p:
+            out, err = p.communicate()
+            self.gui.get_logger().external(out)
+
+        progressbar.stop()
+
+        # logging
+        self.gui.get_logger().info(f"[+] Decompiled apk: {g_current_file}\n")
+
+    #@staticmethod
+    def recompile_apk_callback(self, __none__=None,threaded=False) -> None:
+        if threaded == False: # hack, i'm too lazy to asyncify tkinter
+            thread = Thread(
+                target=self.recompile_apk_callback, 
+                args=(self, True,)
+            )
+            thread.start()
+            return
+
+        global g_current_file, g_apk_parser
+
+        if g_current_file == None:
+            self.gui.get_logger().error(f'[ !] Nothing to re-compile\n')
+            return
+
+        _, file_name = os.path.split(g_current_file)
+
+        recompile_cmd = f'java -Xmx1024m -jar {str(Path.cwd())}\\vendor\\apktool\\apktool.jar' \
+            f" b {str(Path.cwd())}\\decompiled\\{file_name}_decompiled -o {str(Path.cwd())}\\build\\{file_name}_recompiled.apk"
+
+        progressbar = self.gui.get_element_by_opt_id(
+            ECoreElements.PROGRESS_BAR
+        )
+
+        progressbar.start()
+
+        # re-packing apk
+        with subprocess.Popen(
+            recompile_cmd, shell=False, stdout=subprocess.PIPE, 
+            bufsize=1, universal_newlines=True
+        ) as p:
+            out, err = p.communicate()
+            self.gui.get_logger().external(out)
+
+        sign_cmd = f'java -jar {str(Path.cwd())}\\vendor\\apksigner\\apksigner.jar' \
+            f' sign --key {str(Path.cwd())}\\vendor\\apksigner\\testkey.pk8' \
+            f' --cert {str(Path.cwd())}\\vendor\\apksigner\\testkey.x509.pem' \
+            f' {str(Path.cwd())}\\build\\{file_name}_recompiled.apk'
+
+        # signing packed apk
+        with subprocess.Popen(
+            sign_cmd, shell=False, stdout=subprocess.PIPE, 
+            bufsize=1, universal_newlines=True
+        ) as p:
+            out, err = p.communicate()
+            self.gui.get_logger().external(out)
+
+        # check if signed successfully
+        g_apk_parser.set_apk(f'{str(Path.cwd())}\\build\\{file_name}_recompiled.apk')
+
+        if g_apk_parser.is_signed():
+            self.gui.get_logger().info(f"[+] Signed apk: {g_current_file}_recompiled\n")
+        else:
+            self.gui.get_logger().error(f'[ !] Failed to sign apk: {g_current_file}_recompiled\n')
+            progressbar.stop()
+            return
+
+        progressbar.stop()
+
+        # logging
+        self.gui.get_logger().info(f"[+] Re-compiled apk: {g_current_file}_recompiled\n")
